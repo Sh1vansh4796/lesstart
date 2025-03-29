b3688014cbb7dd9a4cdab38eae5ee2cdfdf6029f
Sh1vansh4796 <shivansh4796@gmail.com>
2025-03-29 19:01:53 +0000

    objects done

---
+++ basics/objects2.js
@@ -0,0 +1,42 @@
+//singleton or constructor
+const tinderUser = new Object()
+
+
+tinderUser.id = "22je0912"
+tinderUser.name = "Shivansh"
+tinderUser.isLogged = false
+
+console.log(tinderUser)
+
+const normalUser = {
+    email : "hairynigga69@gmail.com",
+    fullname : {
+        user : {
+            firstname : "Shivansh",
+            lastname : "Gilhotra"
+        }
+    }
+}
+console.log(normalUser.fullname.user.firstname)
+
+// concatenation waala kaam
+
+const obj3 = Object.assign({},tinderUser,normalUser) //{} dena is a good habit as {} is base object mhi denge to obj1 will be
+console.log(obj3)
+
+// much better
+const obj4 = {...tinderUser,...normalUser}
+console.log(obj4)
+
+//obj + array
+
+const userArr = [
+    {name : "shivansh",ID : 912},{name : "shivansh",ID : 914}
+    ,{name : "shivansh",ID : 915},{name : "shivansh",ID : 919}
+    ,{name : "shivansh",ID : 916}
+]
+console.log(userArr[2].ID)
+
+console.log(Object.keys(tinderUser)) // gives an array
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
