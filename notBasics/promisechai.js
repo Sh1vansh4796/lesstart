8533ec0b1afe03e650d47fd2adf2861b669ba5ce
Sh1vansh4796 <shivansh4796@gmail.com>
2025-05-18 05:20:17 +0000

    promising

---
+++ notBasics/promisechai.js
@@ -0,0 +1,42 @@
+const promise1 = new Promise(function(resolve,reject){
+    // do an async task
+    // DB call, N/W calls , cryptography
+    setTimeout(() => {
+        console.log("async task done");
+        resolve()
+    }, 1000);
+})
+
+promise1.then(()=>{
+    console.log("then funtion")
+})
+
+const promise3 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve({username:'random' , email: 'random@mail'});
+    }, 1000 );
+})
+promise3.then((user)=>{
+    console.log('then2 value')
+    console.log(user)
+})
+
+console.log("")
+const promise4 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        let err = false;
+        if(!err) resolve({username : "langda tyagi" , password : "123"})
+        else reject("Something went wrong")
+
+    }, 1000 );
+})
+
+promise4.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((use)=>{
+    console.log(use);
+    
+}).catch((err)=>{
+    console.log(err)
+})
